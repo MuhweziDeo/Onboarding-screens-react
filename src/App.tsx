50723233977a5d51da41f645ecbe5b6f3bf9b775
muhwezideo <aggrey256@gmail.com>
2019-12-24 10:10:11 +0300

    setup project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from 'antd/es/button';
+import logo from './logo.svg';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Button type="primary">Get Started</Button>
+    </div>
+  );
+}
+
+export default App;
