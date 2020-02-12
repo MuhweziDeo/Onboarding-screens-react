cf2f9517ef3bc5e0e7c7ab1b7c28d91b470b1e10
Muhwezi Deo <39124174+MuhweziDeo@users.noreply.github.com>
2020-02-12 03:51:28 +0300

    Update config.tsx

--- src/config.tsx
+++ src/config.tsx
@@ -11,14 +11,14 @@ export interface IFirebaseConfig {
 }
 
 export const firebaseConfig: IFirebaseConfig = Object.freeze({
-    apiKey: "AIzaSyAwhZeKlwO1a06HbWRFR2EDEALXLGlfSkc",
-    authDomain: "the-future-school.firebaseapp.com",
-    databaseURL: "https://the-future-school.firebaseio.com",
-    projectId: "the-future-school",
-    storageBucket: "the-future-school.appspot.com",
-    messagingSenderId: "238112416573",
-    appId: "1:238112416573:web:918df745b370691d68db54",
-    measurementId: "G-2WZ0VP0Q90"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 });
 
 export const config = Object.freeze({
